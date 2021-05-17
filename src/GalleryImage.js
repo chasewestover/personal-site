eb4012a435fa4484660e8bb914cd786a04389bca
Chase Westover <chasejwestover@gmail.com>
2021-05-17 18:42:27 -0500

    undo previous change

--- src/GalleryImage.js
+++ src/GalleryImage.js
@@ -4,7 +4,7 @@ function GalleryImage({ src, id, alt, type }) {
   return (
     <Link to={`${type}/${id}`}>
       <div>
- 
+
         <img src={src} alt={alt} className='masonry-item mb-7' />
       </div>
     </Link>
