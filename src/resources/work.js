095c326288b19092a139a06b7aacc53f28dca706
Chase Westover <chasejwestover@gmail.com>
2021-05-28 14:13:19 -0500

    changed desc for Social Grove

--- src/resources/work.js
+++ src/resources/work.js
@@ -1,7 +1,7 @@
 
 import Jobly from '../vids/Jobly.mp4';
 import Warbler from '../vids/Warbler.mp4';
-import SocialGrove from '../vids/SocialDine.mp4';
+import SocialGrove from '../vids/SocialGrove.mp4';
 
 const work = [
   { src: Jobly, id: 'jobly', title: "Jobly", 
@@ -38,7 +38,7 @@ const work = [
   },
   { src: SocialGrove, id: 'social-grove', title: "Social Grove", 
   technologies: 'React, Json Server',
-  description: [`Social Grove is a social dining experience that lets users suggest dishes to be served and then vote on those dishes to
+  description: [`Social Grove is a social dining experience that lets users suggest dishes to be served to
     determine the menu. It uses React on the frontend and a simple json server on the backend to manage the data`,
 
   `Regular users can add dishes and drinks and vote on their favorites. Admin users can remove items that don't meet the guidelines`], 
