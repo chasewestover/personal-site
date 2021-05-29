eadd69dfe4de1e50cfd9009ac886d8ed7301b177
Chase Westover <chasejwestover@gmail.com>
2021-05-29 18:35:30 -0500

    remove resume for now

--- src/Contact.js
+++ src/Contact.js
@@ -1,11 +1,11 @@
 import {Link} from 'react-router-dom'
 import resume from './files/CWResumé.pdf'
 
+console.log(resume)
+
 function Contact(){
   return (
     <div className='my-5 text-blue-500 text-xl'>
-      <Link to={resume} target="_blank" download>Resumé</Link>
-      <br/>
       <a href='https://www.linkedin.com/in/chasewestover/'>LinkedIn</a>
     </div>
   )
